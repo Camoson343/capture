29a0d877a545f316887d4ab7d6e08236be739b09
Camoson343 <ccdecisions@gmail.com>
2020-11-16 14:06:36 -0800

    fix AboutSection space-between horizontal alignment

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,26 +1,44 @@
-import React from 'react'
-import home1 from '../img/home1.png';
+import React from "react";
+import home1 from "../img/home1.png";
+import styled from "styled-components";
 
 const AboutSection = () => {
-    return (
-        <div className="description">
-            <div className="title">
-                <div className="hide">
-                    <h2>We work to make</h2>
-                </div>
-                <div className="hide">
-                    <h2>your <span>dreams</span> come</h2>
-                </div>
-                <div className="hide">
-                    <h2>true.</h2>
-                </div>
-                <p>Contact us for any photos or vid ideas that you have. We have pros with amazeballs skills.</p>
-                <button>Contact Us</button>
-                <div className="image">
-                    <img src={home1} alt="guy with a camera"/>
-                </div>
-            </div>
+  return (
+    <About>
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2>We work to make</h2>
+          </div>
+          <div className="hide">
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </div>
+          <div className="hide">
+            <h2>true.</h2>
+          </div>
         </div>
-    )
-}
+        <p>
+          Contact us for any photography or videography ideas that you have. We
+          have professionals with amazing skills.
+        </p>
+        <button>Contact Us</button>
+      </div>
+      <div className="image">
+        <img src={home1} alt="guy with a camera" />
+      </div>
+    </About>
+  );
+};
+
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
 export default AboutSection;
