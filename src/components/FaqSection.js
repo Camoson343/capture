180b6ca58bb80919f793043a3b4bf4a83235bf80
Camoson343 <ccdecisions@gmail.com>
2020-11-23 10:41:00 -0800

    finish FAQ animations

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,76 +1,80 @@
-import React from 'react';
-import styled from 'styled-components';
-import {Layout} from '../styles';
+import styled from "styled-components";
+import Toggle from "./Toggle";
+import {Layout} from "../styles";
+import { AnimateSharedLayout } from "framer-motion";
 
 const FaqSection = () => {
     return (
-        <Faq>
-            <h2>Any Questions <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How do I start?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Enim, eligendi.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different Schedule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Enim, eligendi.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different payment methods.</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Enim, eligendi.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different payment methods.</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Enim, eligendi.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-        </Faq>
-    )
-}
+      <Faq>
+        <h2>
+          Any Questions <span>FAQ</span>
+        </h2>
+        <AnimateSharedLayout>
+        <Toggle title="How Do I Start?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+            magnam placeat corporis.
+            </p>
+          </div>
+          </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error,
+              totam.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Diferrent Payment Methods">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, neque.
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="What Products do you offer.">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, neque.
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
+    </Faq>
+  );
+};
 
 const Faq = styled(Layout)`
+  display: block;
+  span {
     display: block;
-    span{
-        display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
     }
-    h2{
-        padding-bottom: 2rem;
-        font-weight: lighter;
-    }
-    .faq-line{
-        background: #cccccc;
-        height: 0.2rem;
-        margin: 2rem 0rem;
-        width: 100%;
-    }
-    .question{
-        padding: 3rem 0rem;
-        cursor: pointer;
-    }
-    .answer{
-        padding: 2rem 0rem;
-        p{
-            padding: 1rem 0rem;
-        }
-    }
+  }
 `;
 
 export default FaqSection;
