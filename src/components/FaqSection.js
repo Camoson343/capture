99e04b52e2b764154e0b25615b95642b4ebf6d0c
Camoson343 <ccdecisions@gmail.com>
2020-11-23 11:12:10 -0800

    finish servicesSection and FAQ scroll animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,10 +2,18 @@ import styled from "styled-components";
 import Toggle from "./Toggle";
 import {Layout} from "../styles";
 import { AnimateSharedLayout } from "framer-motion";
+import { useScroll } from "./useScroll";
+import { scrollReveal } from "../animation";
 
 const FaqSection = () => {
+  const [element, controls] = useScroll();
     return (
-      <Faq>
+      <Faq
+      variants={scrollReveal}
+      ref={element}
+      animate={controls}
+      initial="hidden"
+    >
         <h2>
           Any Questions <span>FAQ</span>
         </h2>
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -6,11 +6,20 @@ import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
 import {Layout, Description, Image} from '../styles';
+import {fade} from '../animation';
 import styled from 'styled-components';
+import { scrollReveal } from "../animation";
+import { useScroll } from "./useScroll";
 
 const ServicesSection = () => {
+  const [element, controls] = useScroll();
   return (
-    <Services>
+    <Services
+      variants={scrollReveal}
+      animate={controls}
+      initial="hidden"
+      ref={element}
+    >
       <Description>
         <h2>
           High <span>quality</span> services
